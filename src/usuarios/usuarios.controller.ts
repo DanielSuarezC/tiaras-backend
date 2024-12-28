0996982a3a5315609639427b2c3408f02dfaea76
Andrés Esala <andresesala25@outlook.com>
2024-12-28 18:42:53 -0500

    (Add) Petición Put para actualizar Usuario

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
 import { UsuarioRegister } from './domain/dto/UsuarioRegister';
 import { UsuariosService } from './services/usuarios.service';
 import { Roles } from 'src/auth/decorators/roles.decorator';
@@ -14,6 +14,13 @@ export class UsuariosController {
     @Post("sign-in")
     @Roles(RolEnum.ADMINISTRADOR)
     signInUsuario(@Body() usuarioRegister: UsuarioRegister) {
-        return this.usuariosService.signInUsuario(usuarioRegister);
+        return this.usuariosService.createUsuario(usuarioRegister);
+    }
+
+    /* Actualizar Usuario */
+    @Put("update/:idUsuario")
+    @Roles(RolEnum.ADMINISTRADOR)
+    updateUsuario(@Param('idUsuario') idUsuario: number, @Body() usuarioRegister: UsuarioRegister) {
+        return this.usuariosService.updateUsuario(idUsuario, usuarioRegister);
     }
 }
