0b621cd1ba975f90593bc2b040579f078ec22c03
Andrés Esala <andresesala25@outlook.com>
2024-12-29 17:49:31 -0500

    (Create) Controlador base de Clientes

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { ClientesService } from './services/clientes.service';
+import { ClienteRegister } from './domain/dto/ClienteRegister.dto';
+import { RolEnum } from 'src/auth/domain/dto/roles.enum';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('clientes')
-export class ClientesController {}
+@Roles(RolEnum.ADMINISTRADOR)
+export class ClientesController {
+    constructor(
+        private readonly clientesService: ClientesService
+    ) {}
+
+    @Get()
+    async findAll() {
+        return await this.clientesService.findAll();
+    }
+
+    @Get(':idCliente')
+    async findOne(@Param('idCliente') id: number) {
+        return await this.clientesService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() cliente: ClienteRegister) {
+        return await this.clientesService.create(cliente);
+    }
+
+    @Put(':idCliente')
+    async update(@Param('idCliente') idCliente: number, @Body() cliente: ClienteRegister) {
+        return await this.clientesService.update(idCliente, cliente);
+    }
+}
