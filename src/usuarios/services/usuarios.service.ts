e0d899302ee21b25d21956d93203a7d1dfbc620d
Andrés Esala <andresesala25@outlook.com>
2024-12-29 17:44:55 -0500

    (Update) Manejo de métodos con Transacciones y adición del método UpdateUsuario

--- src/usuarios/services/usuarios.service.ts
+++ src/usuarios/services/usuarios.service.ts
@@ -1,6 +1,6 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { UsuarioEntity } from '../domain/entites/Usuario.entity';
 import { UsuarioRegister } from '../domain/dto/UsuarioRegister';
 import { UsuarioSignIn } from 'src/auth/domain/dto/UsuarioSignIn.dto';
@@ -10,33 +10,63 @@ import { AuthService } from 'src/auth/services/auth.service';
 @Injectable()
 export class UsuariosService {
     constructor(
+        @InjectEntityManager('main')
+        private readonly entityManager: EntityManager,
         @InjectRepository(UsuarioEntity, 'main')
         private readonly usuariosRepository: Repository<UsuarioEntity>,
         private readonly authService: AuthService
     ) {}
 
+    /* Obtener todos los Usuarios */
+    async getUsuarios(): Promise<UsuarioEntity[]> {
+        return await this.usuariosRepository.find();
+    }
+
     /* Registrar nuevo Usuario */
-    async signInUsuario(usuario: UsuarioRegister): Promise<any> {
-        const usuarioSignIn: UsuarioSignIn = new UsuarioSignIn();
-        usuarioSignIn.email = usuario.email;
-        usuarioSignIn.password = usuario.password;
-        usuarioSignIn.rol = usuario.rol;
+    async createUsuario(usuario: UsuarioRegister): Promise<any> {
+        try {
+            await this.entityManager.transaction(async (transactionEntityManager: EntityManager) => {
+                const usuarioSignIn: UsuarioSignIn = new UsuarioSignIn();
+                usuarioSignIn.email = usuario.email;
+                usuarioSignIn.password = usuario.password;
+                usuarioSignIn.rol = usuario.rol;
 
-        const idUsuarioRegistered: number = await this.authService.signIn(usuarioSignIn);
-        if (!idUsuarioRegistered) throw BadRequestException;
+                const idUsuarioRegistered: number = await this.authService.signIn(usuarioSignIn);
+                if (!idUsuarioRegistered) throw BadRequestException;
 
-        const usuarioEntity: UsuarioEntity = new UsuarioEntity();
-        usuarioEntity.idUsuario = idUsuarioRegistered;
+                const usuarioEntity: UsuarioEntity = new UsuarioEntity();
+                usuarioEntity.idUsuario = idUsuarioRegistered;
+                usuarioEntity.nombre = usuario.nombre;
+                usuarioEntity.apellidos = usuario.apellidos;
+                usuarioEntity.telefono = usuario.telefono;
+                usuarioEntity.email = usuario.email;
+                
+                return await this.usuariosRepository.save(usuarioEntity);
+            });
+        } catch (error) {
+            throw new BadRequestException(error.message);
+        }
+    }
+
+    /* Actualizar Usuario */
+    async updateUsuario(idUsuario: number, usuario: UsuarioRegister): Promise<any> {
+
+        const usuarioEntity: UsuarioEntity = await this.usuariosRepository.findOneBy({ idUsuario: idUsuario });
+        if (!usuarioEntity) throw NotFoundException;
+
         usuarioEntity.nombre = usuario.nombre;
         usuarioEntity.apellidos = usuario.apellidos;
         usuarioEntity.telefono = usuario.telefono;
         usuarioEntity.email = usuario.email;
-        
-        return await this.usuariosRepository.save(usuarioEntity);
-    }
 
-    /* Obtener todos los Usuarios */
-    async getUsuarios(): Promise<UsuarioEntity[]> {
-        return await this.usuariosRepository.find();
+        const usuarioSignIn: UsuarioSignIn = new UsuarioSignIn();
+        usuarioSignIn.email = usuario.email;
+
+        if (usuario.password.trim() !== '') usuarioSignIn.password = usuario.password;
+        usuarioSignIn.rol = usuario.rol;
+
+        const idUsuarioRegistered: number = await this.authService.signIn(usuarioSignIn);
+
+        return await this.usuariosRepository.save(usuarioEntity);
     }
 }
