67dcef88ae75036aab843b4dd27e2d1789b51af6
Andrés Esala <andresesala25@outlook.com>
2024-12-29 17:50:20 -0500

    (Update) Adición de Import para `ClienteEntity` con TypeOrmModule para uso de Repository

--- src/clientes/clientes.module.ts
+++ src/clientes/clientes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientesController } from './clientes.controller';
 import { ClientesService } from './services/clientes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClienteEntity } from './domain/entities/Cliente.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ClienteEntity], 'main')],
   controllers: [ClientesController],
   providers: [ClientesService]
 })
