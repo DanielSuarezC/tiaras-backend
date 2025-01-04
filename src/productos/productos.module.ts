79e8a0cf43be22ac9674eab263b7fb263c876ab4
Andrés Esala <andresesala25@outlook.com>
2025-01-03 20:39:40 -0500

    (Update) Configuración de carpeta destino para almacenar las imágenes subidas

--- src/productos/productos.module.ts
+++ src/productos/productos.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ProductosService } from './services/productos.service';
 import { ProductosController } from './productos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductoEntity } from './domain/entities/Producto.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { CategoriaEntity } from 'src/categorias/domain/entites/Categoria.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ProductoEntity, CategoriaEntity], 'main'),
+    MulterModule.registerAsync({
+      useFactory: () => ({
+        dest: './uploads'
+      })
+    })
+  ],
   controllers: [ProductosController],
   providers: [ProductosService]
 })
