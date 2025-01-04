2f3973c459572bb30ae2c399a0f8af7188bbe85b
Andrés Esala <andresesala25@outlook.com>
2025-01-03 20:42:56 -0500

    (Update) Métodos para buscar por Id, crear, actualizar y eliminar

--- src/productos/services/productos.service.ts
+++ src/productos/services/productos.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductoEntity } from '../domain/entities/Producto.entity';
-import { Repository } from 'typeorm';
+import { Repository, In } from 'typeorm';
+import { ProductoRegister } from '../domain/dto/ProductoRegister.dto';
+import { CategoriaEntity } from 'src/categorias/domain/entites/Categoria.entity';
 
 @Injectable()
 export class ProductosService {
     constructor(
         @InjectRepository(ProductoEntity, 'main')
-        private readonly productoRepository: Repository<ProductoEntity>
+        private readonly productoRepository: Repository<ProductoEntity>,
+        @InjectRepository(CategoriaEntity, 'main')
+        private readonly categoriaRepository: Repository<CategoriaEntity>
     ) {}
 
     async findAll(): Promise<[ProductoEntity[], number]> {
@@ -18,7 +22,35 @@ export class ProductosService {
         return await this.productoRepository.findOneBy({ idProducto });
     }
 
-    async create(producto: ProductoEntity): Promise<ProductoEntity> {
+    async existsByIds(ids: number[]): Promise<boolean> {
+        return await this.productoRepository.existsBy({ idProducto: In(ids) });
+    }
+
+    async findByIds(ids: number[]): Promise<ProductoEntity[]> {
+        return await this.productoRepository.findBy({ idProducto: In(ids) });
+    }
+
+    async create(producto: ProductoRegister, fileNames: string[]): Promise<ProductoEntity> {
+        const productoEntity = new ProductoEntity();
+        productoEntity.nombre = producto.nombre;
+        productoEntity.descripcion = producto.descripcion;
+        productoEntity.precio = producto.precio;
+        productoEntity.imagenes = fileNames;
+
+        const categoriasExists: boolean = await this.categoriaRepository.existsBy({ idCategoria: In(producto.categorias) });
+        if (!categoriasExists) throw new Error('Alguna de las categorías no existe');
+
+        const categorias: CategoriaEntity[] = await this.categoriaRepository.findBy({ idCategoria: In(producto.categorias) });
+        productoEntity.categorias = categorias;
+
+        return await this.productoRepository.save(productoEntity);
+    }
+
+    async update(producto: ProductoEntity): Promise<ProductoEntity> {
         return await this.productoRepository.save(producto);
+    }
+
+    async delete(idProducto: number): Promise<void> {
+        await this.productoRepository.delete(idProducto);
     }
 }
