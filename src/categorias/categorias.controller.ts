d992df1aa63e28a310e8935405dc9dd3dbe28d35
Andrés Esala <andresesala25@outlook.com>
2025-01-03 20:30:13 -0500

    (Update) Corrección de EndPoints

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CategoriasService } from './services/categorias.service';
+import { CategoriaRegister } from './domain/dto/CategoriaRegister.dto';
 
 @Controller('categorias')
 export class CategoriasController {
     constructor(
         private readonly categoriasService: CategoriasService
     ) {}
-
+ 
     @Get()
     async findAll() {
         return this.categoriasService.findAll();
@@ -15,5 +16,15 @@ export class CategoriasController {
     @Get(':id')
     async findOne(idCategoria: number) {
         return this.categoriasService.findOne(idCategoria);
+    }
+
+    @Post()
+    async create(@Body() categoria: CategoriaRegister) {
+        return this.categoriasService.create(categoria);
+    }
+
+    @Put(':idCategoria')
+    async update(@Param('idCategoria') idCategoria: number, @Body() categoria: CategoriaRegister) {
+        return this.categoriasService.update(idCategoria, categoria);
     }
 }
