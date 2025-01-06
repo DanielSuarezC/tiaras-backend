2c088896ba6d10e0316dca06281322d54223b0f2
Andrés Esala <andresesala25@outlook.com>
2025-01-06 14:46:15 -0500

    (Updated) Actualización de import de RolEnum

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from "@nestjs/common";
-import { RolEnum } from "../domain/dto/roles.enum";
+import { RolEnum } from "../dto/roles.enum";
 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: RolEnum[]) => SetMetadata(ROLES_KEY, roles);
